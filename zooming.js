f25320feed6125a76b46f9c3bb49d120da7828a0
bluziDev <austinbarton99@outlook.com>
2024-07-19 17:09:43 -0600

    -fixed canvas placement and zoom curve

--- zooming.js
+++ zooming.js
@@ -26,7 +26,7 @@ export function pan(cam,prev,curr){
     return {cam: new_cam,mouse: new_curr};
 }
 export function slider_to_zoom(input){
-    let real_zoom = parseFloat(input) + 0.368;
-    real_zoom = Math.pow(real_zoom,real_zoom) - 0.692;
+    let real_zoom = parseFloat(input);// + 0.368;
+    real_zoom = Math.pow(2,real_zoom);// - 0.692;
     return real_zoom;
 }
